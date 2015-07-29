8ee16aa89b1b5b25769b9da27605171afb250c42
Laney Smith <lsmith.unc@gmail.com>
2015-07-29 17:37:37 -0600

    minor changes

--- js/app.js
+++ js/app.js
@@ -1,29 +1,28 @@
+// Enemies our player must avoid: sets up Enemy object & initializes
+// variables for each instance of the object
+var Enemy = function() {
 
- // Enemies our player must avoid: sets up Enemy object & initializes
- // variables for each instance of the object
- var Enemy = function() {
+    this.sprite = 'images/enemy-bug.png'; // Enemy bug sprite
+    this.x = 0; // Initial x coord
+    this.y = 63; // Initial y coord
+    this.width = 101; // Set width of sprite
+    this.height = 83; // Set height of sprite
 
-    this.sprite = 'images/enemy-bug.png';  // Enemy bug sprite
-    this.x = 0;                            // Initial x coord
-    this.y = 63;                           // Initial y coord
-    this.width = 101;                      // Set width of sprite
-    this.height = 83;                      // Set height of sprite
-
-    this.dt = getRandomArbitrary(2, 8);   // Random speed
+    this.dt = getRandomArbitrary(2, 8); // Random speed
 
     function getRandomArbitrary(min, max) {
-      return Math.random() * (max - min) + min;
+        return Math.random() * (max - min) + min;
     }
 };
 
 
- // Update the enemy's position, required method for game
- // Parameter: dt, a time delta between ticks
- Enemy.prototype.update = function(dt) {
-     // If statement compares enemy position with canvas width to determine if
-     // enemy traveled off canvas. If true, function resets enemy position.
-     // If false, enemy continues to move along x-axis at the random
-     // speed (dt) associated with the enemy instance.
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // If statement compares enemy position with canvas width to determine if
+    // enemy traveled off canvas. If true, function resets enemy position.
+    // If false, enemy continues to move along x-axis at the random
+    // speed (dt) associated with the enemy instance.
     if (this.x > ctx.canvas.width) {
         this.x = -101;
     } else {
@@ -43,11 +42,11 @@
             if (player.x + 30 > allEnemies[n].x && player.x < allEnemies[n].x + allEnemies[n].width - 30) {
                 if (player.y < allEnemies[n].y + 20 && player.y > allEnemies[n].y - 20) {
                     // Reset player to initial coordinates.
-                    Player.prototype.reset();
+                    player.reset();
                     // If final score is greater than current high score, record
                     // new high score.
                     if (score > maxScore) {
-                      maxScore = score;
+                        maxScore = score;
                     }
                     // Reset score to 0.
                     score = 0;
@@ -80,18 +79,17 @@ var Player = function() {
         'images/char-pink-girl.png',
         'images/char-princess-girl.png',
     ];
-    this.currentSpriteIndex = 0;      // Initiates the index value for the sprite array
-    this.x = playerInitialX;          // Initiates the player's x coordinate
-    this.y = playerInitialY;          // Initiates the player's y coordinate
-    this.dt = 40;                     // Sets the speed factor.
-    this.width = 101;                 // Sets the width of the sprite.
-    this.height = 83;                 // Sets the height of the sprite.
+    this.currentSpriteIndex = 0; // Initiates the index value for the sprite array
+    this.x = playerInitialX; // Initiates the player's x coordinate
+    this.y = playerInitialY; // Initiates the player's y coordinate
+    this.dt = 40; // Sets the speed factor.
+    this.width = 101; // Sets the width of the sprite.
+    this.height = 83; // Sets the height of the sprite.
 };
 
 // Update the player's position, required method for game
 // Parameter: dt, a time delta between ticks
-Player.prototype.update = function(dt) {
-};
+Player.prototype.update = function(dt) {};
 
 // Draw the player on the screen using the drawImage function.
 // Also displays the directions, scoreboard, & high score.
@@ -106,7 +104,7 @@ Player.prototype.render = function() {
 // Move player across canvas using up, down, left, & right arrow keys.
 // Press space bar key to iterate through characters in Player.sprite array.
 Player.prototype.handleInput = function(keyStroke) {
-   if (keyStroke === 'left') {
+    if (keyStroke === 'left') {
         // If statement checks to see if player is currently at the furthest
         // left position on the canvas. If not, x coordinate is changed.
         if (this.x - 101 >= 0) {
@@ -128,13 +126,13 @@ Player.prototype.handleInput = function(keyStroke) {
         if (this.y > 83) {
             this.y = this.y - 83;
         } else {
-            Player.prototype.reset();
+            this.reset();
             score = score + 1;
         }
     }
     if (keyStroke === 'down') {
-      // If statement checks to see if player is currently at the furthest
-      // bottom position on the canvas. If not, y coordinate is changed.
+        // If statement checks to see if player is currently at the furthest
+        // bottom position on the canvas. If not, y coordinate is changed.
         if (this.y + 83 < ctx.canvas.height - 200) {
             this.y = this.y + 83;
         }
@@ -153,10 +151,10 @@ Player.prototype.handleInput = function(keyStroke) {
 
 // Instantiate enemy objects
 var allEnemies = [
-        new Enemy(),
-        new Enemy(),
-        new Enemy()
-    ];
+    new Enemy(),
+    new Enemy(),
+    new Enemy()
+];
 
 // Change y coordinates for 2nd & 3rd instances of Enemy
 allEnemies[1].y = allEnemies[0].y + 83;
@@ -186,18 +184,13 @@ document.addEventListener('keyup', function(e) {
 // Reset player's position after collision or successful crossing.
 // Called by checkCollisions & Player.handleInput
 Player.prototype.reset = function() {
-      player.x = playerInitialX;      //player's starting x coordinate
-      player.y = playerInitialY;      //player's starting y coordinate
-  }
-
-//function resetPlayer() {
-//    player.x = playerInitialX;      //player's starting x coordinate
-//    player.y = playerInitialY;      //player's starting y coordinate
-//}
+    this.x = playerInitialX; //player's starting x coordinate
+    this.y = playerInitialY; //player's starting y coordinate
+};
 
 
-var score = 0;                      // Initialize score
-var maxScore = 0;                   // Initialize high score
+var score = 0; // Initialize score
+var maxScore = 0; // Initialize high score
 
 function scoreBoard() {
     ctx.font = 'bold 24px sans-serif';
@@ -217,6 +210,6 @@ function directions() {
     ctx.font = '14px sans-serif';
     ctx.textAlign = 'center';
     ctx.fillStyle = 'white';
-    ctx.fillText('Use the arrow keys to dodge the beetles & move your character to the water.', 505/2, 562);
-    ctx.fillText('Press the space bar key to choose a different character.', 505/2, 580);
+    ctx.fillText('Use the arrow keys to dodge the beetles & move your character to the water.', 505 / 2, 562);
+    ctx.fillText('Press the space bar key to choose a different character.', 505 / 2, 580);
 }
